f49bc4adb9e4b64cce6003875f4e69d44c0925aa
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-12-09 23:17:24 +0300

    more debugging

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@
 -- name, string (255 characters)
 -- country, enumeration of countries: US, CO and TN, never null
 
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users(
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
