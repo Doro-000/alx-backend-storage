e4b1ad9006be66c90e0e731fa8285fc25dd86389
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-12-15 23:30:43 +0300

    debugging

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,53 @@ from typing import Callable, Union
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    decorator to count how many times a function has been called
+    """
+    @wraps(method)
+    def incr(self, *args, **kwargs):
+        """
+        increments the count
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return incr
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    decorator to store input and output history of a method
+    """
+    @wraps(method)
+    def store_history(self, *args, **kwargs):
+        """
+        stores input and output to redis
+        """
+        self._redis.rpush(method.__qualname__ + ":inputs", str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(method.__qualname__ + ":outputs", str(result))
+        return result
+    return store_history
+
+
+def replay(function: callable):
+    """
+    replays the call history of a function
+    """
+    my_redis = redis.Redis()
+    input_key = function.__qualname__ + ":inputs"
+    output_key = function.__qualname__ + ":outputs"
+    inputs = my_redis.lrange(input_key, 0, -1)
+    outputs = my_redis.lrange(output_key, 0, -1)
+    print(
+        "{} was called {} times:".format(
+            function.__qualname__,
+            my_redis.llen(input_key)))
+    for op in zip(inputs, outputs):
+        print("function.__qualname__({}) -> {}".format(op[0], op[1]))
+
+
 class Cache:
     """
     Cache class to handle caching using redis
@@ -20,34 +67,6 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    def count_calls(method: Callable) -> Callable:
-        """
-        decorator to count how many times a function has been called
-        """
-        @wraps(method)
-        def incr(self, *args, **kwargs):
-            """
-            increments the count
-            """
-            self._redis.incr(method.__qualname__)
-            return method(self, *args, **kwargs)
-        return incr
-
-    def call_history(method: Callable) -> Callable:
-        """
-        decorator to store input and output history of a method
-        """
-        @wraps(method)
-        def store_history(self, *args, **kwargs):
-            """
-            stores input and output to redis
-            """
-            self._redis.rpush(method.__qualname__ + ":inputs", str(args))
-            result = method(self, *args, **kwargs)
-            self._redis.rpush(method.__qualname__ + ":outputs", str(result))
-            return result
-        return store_history
 
     @call_history
     @count_calls
@@ -80,20 +99,3 @@ class Cache:
         Get value of key from redis, return the value as an int
         """
         return self.get(key, int)
-
-
-def replay(function: callable):
-    """
-    replays the call history of a function
-    """
-    my_redis = redis.Redis()
-    input_key = function.__qualname__ + ":inputs"
-    output_key = function.__qualname__ + ":outputs"
-    inputs = my_redis.lrange(input_key, 0, -1)
-    outputs = my_redis.lrange(output_key, 0, -1)
-    print(
-        "{} was called {} times:".format(
-            function.__qualname__,
-            my_redis.llen(input_key)))
-    for op in zip(inputs, outputs):
-        print("function.__qualname__({}) -> {}".format(op[0], op[1]))
